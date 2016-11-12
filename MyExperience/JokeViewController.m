6830677fdcb801da942a156b78429f0da839044a
xiaoyang <1083394272@qq.com>
2016-11-12 23:24:35 +0800

    wx change

--- MyExperience/JokeViewController.m
+++ MyExperience/JokeViewController.m
@@ -11,12 +11,6 @@
 #import "JuHeService.h"
 #import "UIScrollView+MJRefresh.h"
 
-#import "WXApi.h"
-
-#import "WechatAuthSDK.h"
-
-#import "WXApiObject.h"
-
 
 
 
@@ -36,35 +30,6 @@
 #define PAGE_START  1
 #define COUNT_ONE_PAGE  20
 
--(void)dealloc{
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"WXShare" object:nil];
-}
-
-- (instancetype)init {
-    self = [super init];
-    if(self) {
-        
-        // 接收分享回调通知
-        //监听通知
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getOrderPayResult:) name:@"WXShare" object:nil];
-        
-        // 检查是否装了微信
-        if ([WXApi isWXAppInstalled])
-        {
-            
-        }
-    }
-    return self;
-}
-
-- (void)getOrderPayResult:(NSNotification *)notification
-{
-    // 注意通知内容类型的匹配
-    if (notification.object == 0)
-    {
-        NSLog(@"分享成功");
-    }
-}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -75,7 +40,7 @@
     //一开始就下拉刷新
     [self.dataTableView beginReload];
     
-    [self setRightBarButton];
+//    [self setRightBarButton];
     
     // Do any additional setup after loading the view from its nib.
 }
@@ -120,63 +85,7 @@
 
 - (void)clickWXShareButton{
     
-    /**
-     scene: 发送的目标场景,可以选择发送到会话(WXSceneSession)或者朋友圈(WXSceneTimeline),默认发送到会话.
-     1.分享或收藏的目标场景，通过修改scene场景值实现。
-     2.发送到聊天界面——WXSceneSession
-     3.发送到朋友圈——WXSceneTimeline
-     4.添加到微信收藏——WXSceneFavorite
-     */
-    
-    /** bText:
-     发送消息的类型.包括文本消息和多媒体消息两种.两者只能选择其一.不能同时发送文本和多媒体消息.
-     
-     */
-
-    //1,分享文字
-//    SendMessageToWXReq * req = [[SendMessageToWXReq alloc] init];
-//    req.text = @"分享的内容";
-//    req.bText = YES;
-//    req.scene = WXSceneSession;
-//    [WXApi sendReq:req];
-    
-    //2.图片类型分享
-    /**  WXMediaMessage 多媒体分享的类
-     1. setThumbImage 设置缩略图
-     */
-//    WXMediaMessage * message = [WXMediaMessage message];
-//    [message setThumbImage:[UIImage imageNamed:@"black"]];
-//    
-//    WXImageObject * imageObject = [WXImageObject object];
-//    NSString * filePath = [[NSBundle mainBundle] pathForResource:@"seeall@1x" ofType:@"png"];
-//    imageObject.imageData = [NSData dataWithContentsOfFile:filePath];
-//    message.mediaObject = imageObject;
-//    
-//    SendMessageToWXReq * req = [[SendMessageToWXReq alloc] init];
-//    req.bText = NO;
-//    req.message = message;
-//    req.scene = WXSceneSession;
-//    [WXApi sendReq:req];
-    
-    //3.网页类型分享
-    WXMediaMessage * message = [WXMediaMessage message];
-    message.title = @"应用分享";
-    message.description = @"这是一个挺不错的APP，赶紧下载吧！";
-//    [message setThumbImage:[UIImage imageNamed:@"seeall@1x"]];
-    
-    WXWebpageObject * webpageObject = [WXWebpageObject object];
-    webpageObject.webpageUrl = @"http://itunes.apple.com/cn/app/id1167035979";
-    message.mediaObject = webpageObject;
-    
-    SendMessageToWXReq * req = [[SendMessageToWXReq alloc] init];
-    req.bText = NO;
-    
-    req.message = message;
-    req.scene = WXSceneSession;
-    
-    [WXApi sendReq:req];
-    
-}
+    }
 
 
 - (void)didReceiveMemoryWarning {
--- MyExperience/MineViewController.m
+++ MyExperience/MineViewController.m
@@ -12,6 +12,12 @@
 
 #import "ChatViewController.h"
 
+#import "WXApi.h"
+
+#import "WechatAuthSDK.h"
+
+#import "WXApiObject.h"
+
 
 
 
@@ -25,6 +31,37 @@
 
 @implementation MineViewController
 
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"WXShare" object:nil];
+}
+
+- (instancetype)init {
+    self = [super init];
+    if(self) {
+        
+        // 接收分享回调通知
+        //监听通知
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getOrderPayResult:) name:@"WXShare" object:nil];
+        
+        // 检查是否装了微信
+        if ([WXApi isWXAppInstalled])
+        {
+            
+        }
+    }
+    return self;
+}
+
+- (void)getOrderPayResult:(NSNotification *)notification
+{
+    // 注意通知内容类型的匹配
+    if (notification.object == 0)
+    {
+        NSLog(@"分享成功");
+    }
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"个人中心";
@@ -33,6 +70,67 @@
     _discoverList = [[NSArray alloc] initWithObjects:oneArray,twoArray, nil];
     
     // Do any additional setup after loading the view from its nib.
+}
+
+- (void)WXShare{
+    
+    /**
+     scene: 发送的目标场景,可以选择发送到会话(WXSceneSession)或者朋友圈(WXSceneTimeline),默认发送到会话.
+     1.分享或收藏的目标场景，通过修改scene场景值实现。
+     2.发送到聊天界面——WXSceneSession
+     3.发送到朋友圈——WXSceneTimeline
+     4.添加到微信收藏——WXSceneFavorite
+     */
+    
+    /** bText:
+     发送消息的类型.包括文本消息和多媒体消息两种.两者只能选择其一.不能同时发送文本和多媒体消息.
+     
+     */
+    
+    //1,分享文字
+    //    SendMessageToWXReq * req = [[SendMessageToWXReq alloc] init];
+    //    req.text = @"分享的内容";
+    //    req.bText = YES;
+    //    req.scene = WXSceneSession;
+    //    [WXApi sendReq:req];
+    
+    //2.图片类型分享
+    /**  WXMediaMessage 多媒体分享的类
+     1. setThumbImage 设置缩略图
+     */
+    //    WXMediaMessage * message = [WXMediaMessage message];
+    //    [message setThumbImage:[UIImage imageNamed:@"black"]];
+    //
+    //    WXImageObject * imageObject = [WXImageObject object];
+    //    NSString * filePath = [[NSBundle mainBundle] pathForResource:@"seeall@1x" ofType:@"png"];
+    //    imageObject.imageData = [NSData dataWithContentsOfFile:filePath];
+    //    message.mediaObject = imageObject;
+    //
+    //    SendMessageToWXReq * req = [[SendMessageToWXReq alloc] init];
+    //    req.bText = NO;
+    //    req.message = message;
+    //    req.scene = WXSceneSession;
+    //    [WXApi sendReq:req];
+    
+    //3.网页类型分享
+    WXMediaMessage * message = [WXMediaMessage message];
+    message.title = @"应用分享";
+    message.description = @"这是一个挺不错的APP，赶紧下载吧！";
+    //    [message setThumbImage:[UIImage imageNamed:@"seeall@1x"]];
+    
+    WXWebpageObject * webpageObject = [WXWebpageObject object];
+    webpageObject.webpageUrl = @"http://itunes.apple.com/cn/app/id1167035979";
+    message.mediaObject = webpageObject;
+    
+    SendMessageToWXReq * req = [[SendMessageToWXReq alloc] init];
+    req.bText = NO;
+    
+    req.message = message;
+    req.scene = WXSceneSession;
+    
+    [WXApi sendReq:req];
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -105,7 +203,8 @@
            
         }else if ([weakCell.valueLabel.text isEqualToString:@"微信分享"]){
             
-            NSLog(@"2");
+            [self WXShare];
+            
         }
     };
     
